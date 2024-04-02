21bc08692fe6bde20dacb1d202012e3e374da47b
illyalane <lane_illya@yahoo.com>
2024-04-02 19:59:14 -0400

    completed lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,43 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  
+  def __init__(self, value = ""): 
+       
+     self.value = value
+
+  @property
+  def value(self):
+    return self._value
+
+  @value.setter
+  def value(self, val):
+    if isinstance(val, str):
+      self._value = val
+    else:
+      print("The value must be a string.") 
+
+  def is_sentence(check_p):
+    if check_p.value.endswith("."):
+      return True
+    else:
+      return False
+
+  def is_question(check_q):
+    if check_q.value.endswith("?"):
+      return True
+    else:
+      return False
+
+  def is_exclamation(check_e):
+    if check_e.value.endswith("!"):
+      return True
+    else:
+      return False
+
+  def count_sentences(check_s):
+    check_s.value = check_s.value.replace("?", ".")
+    check_s.value = check_s.value.replace("!", ".")
+    sentences = check_s.value.split(".")
+    sentences = [sentence for sentence in sentences if sentence]  # Remove empty sentences
+    return len(sentences)    
